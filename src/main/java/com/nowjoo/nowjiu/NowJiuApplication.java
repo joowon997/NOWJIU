14eb4abcaee63c6ec0b2017bdf9c207d13bc2438
MEGA-IT <MEGA@gamail.com>
2024-10-02 21:19:55 +0900

    [정주원] - 주짓수 쇼핑몰 프로젝트(NOWJIU) 생성

---
+++ src/main/java/com/nowjoo/nowjiu/NowJiuApplication.java
@@ -0,0 +1,13 @@
+package com.nowjoo.nowjiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NowJiuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NowJiuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nowjoo/nowjiu/NowJiuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nowjoo.nowjiu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NowJiuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
