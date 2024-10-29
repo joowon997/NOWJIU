daacb281d0794a9526658336c9a3c757dbede63a
Joowon Jung <jungmincheor@gmail.com>
2024-10-29 17:10:15 +0900

    [정주원] - 주문내역 개발 중

--- src/main/java/com/nowjoo/nowjiu/order/dto/UserOrderHistory.java
+++ src/main/java/com/nowjoo/nowjiu/order/dto/UserOrderHistory.java
@@ -1,5 +1,14 @@
 package com.nowjoo.nowjiu.order.dto;
 
+import java.util.List;
+
+import com.nowjoo.nowjiu.order.domain.Order;
+
 public class UserOrderHistory {
 
+	private int orderListId;
+	private int goodsId;
+	private int orderId;
+	
+	private List<Order> orderList;
 }
