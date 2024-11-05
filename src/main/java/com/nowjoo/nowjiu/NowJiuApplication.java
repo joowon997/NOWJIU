f0611968fcaf7349233e4da7f3f6e6026145c438
MEGA-IT <MEGA@gamail.com>
2024-11-05 20:16:35 +0900

    [정주원] - 배포 코드 작성

--- src/main/java/com/nowjoo/nowjiu/NowJiuApplication.java
+++ src/main/java/com/nowjoo/nowjiu/NowJiuApplication.java
@@ -2,9 +2,10 @@ package com.nowjoo.nowjiu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class NowJiuApplication {
+public class NowJiuApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NowJiuApplication.class, args);
--- src/main/java/com/nowjoo/nowjiu/config/WebMvcConfig.java
+++ src/main/java/com/nowjoo/nowjiu/config/WebMvcConfig.java
@@ -14,7 +14,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/images/**")
-		.addResourceLocations("file:///" + FileManager.FILE_UPLOAD_PATH + "/");
+		// 리눅스
+		.addResourceLocations("file://" + FileManager.FILE_UPLOAD_PATH + "/");
+		// 윈도우
+//		.addResourceLocations("file:///" + FileManager.FILE_UPLOAD_PATH + "/");
 	}
 	
 	@Override
